d7bd35a25ae08e2baf813fcc33412f3455e134f9
francois-yang <francois.yang@edu.univ-fcomte.fr>
2024-11-08 14:17:59 +0100

    Ajout du script JS pour le TP

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,14 @@
-// TODO
+const collH2 = document.querySelectorAll("h2")
+const aMettreEnRouge = document.querySelector("#a-mettre-en-rouge")
+const enRougeSuiteAClick = document.querySelector("#en-rouge-suite-a-click")
+const Enrouge1 = document.querySelector = ("Trouvez tous les 'H2' du document") 
+
+enRougeSuiteAClick.addEventListener("click", (evt)=>{
+    enRougeSuiteAClick.style.color = "red"}); 
+
+aMettreEnRouge.style.color = "red" 
+
+collH2.forEach((elm)=>{
+    elm.addEventListener("click", (evt)=>{
+        evt.target.style.color = "red";});
+}); 
